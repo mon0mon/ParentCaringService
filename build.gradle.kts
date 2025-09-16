9153ea8681ee8242baeb29fd609a2d7b4bacd86d
mon0mon <mon0mon@naver.com>
2025-09-16 21:48:42 +0900

    project: #2 외부 라이브러리 의존성 추가 - JWT - RestDocs, OpenAPI Generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("com.epages.restdocs-api-spec") version "0.19.4"
+    id("org.openapi.generator") version "7.11.0"
 }
 
 group = "com.lumanlab"
@@ -33,15 +35,30 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+    implementation("io.jsonwebtoken:jjwt-impl:0.13.0")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.13.0")
+
     compileOnly("org.projectlombok:lombok")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
     runtimeOnly("org.postgresql:postgresql")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
+
+    // RestDocs
+    testImplementation("com.epages:restdocs-api-spec:0.19.4")
+    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.19.4")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
