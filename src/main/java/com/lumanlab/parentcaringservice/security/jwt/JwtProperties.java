b1428099f044ece81f34ab1456d41e17a1cf6cb0
mon0mon <mon0mon@naver.com>
2025-09-18 10:47:01 +0900

    feat: #4 스프링 시큐리티 및 JWT 로그인 설정 - JWT, JWK 환경 설정 추가 - JwtProperties 프로퍼티 추가

---
+++ src/main/java/com/lumanlab/parentcaringservice/security/jwt/JwtProperties.java
@@ -0,0 +1,58 @@
+package com.lumanlab.parentcaringservice.security.jwt;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private AccessToken accessToken = new AccessToken();
+    private RefreshToken refreshToken = new RefreshToken();
+    private Key key = new Key();
+    private String issuer;
+
+    @Data
+    public static class AccessToken {
+        private long expirationTime; // 초 단위
+
+        public Duration getExpirationDuration() {
+            return Duration.ofSeconds(expirationTime);
+        }
+
+        public long getExpirationTimeMillis() {
+            return expirationTime * 1000;
+        }
+    }
+
+    @Data
+    public static class RefreshToken {
+        private long expirationTime; // 초 단위
+
+        public Duration getExpirationDuration() {
+            return Duration.ofSeconds(expirationTime);
+        }
+
+        public long getExpirationTimeMillis() {
+            return expirationTime * 1000;
+        }
+    }
+
+    @Data
+    public static class Key {
+        private long rotationInterval; // 초 단위
+        private int maxKeys;
+
+        public Duration getRotationIntervalDuration() {
+            return Duration.ofSeconds(rotationInterval);
+        }
+
+        public long getRotationIntervalMillis() {
+            return rotationInterval * 1000;
+        }
+    }
+}
