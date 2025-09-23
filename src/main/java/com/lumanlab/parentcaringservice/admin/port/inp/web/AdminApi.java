aaf513aaa04d2eec80d3cc527401e2a2db07513d
mon0mon <mon0mon@naver.com>
2025-09-23 16:38:49 +0900

    feat: #11 어드민 임퍼소네이션 - 어드민 임퍼소네이션 API 추가

---
+++ src/main/java/com/lumanlab/parentcaringservice/admin/port/inp/web/AdminApi.java
@@ -0,0 +1,33 @@
+package com.lumanlab.parentcaringservice.admin.port.inp.web;
+
+import com.lumanlab.parentcaringservice.admin.application.service.AdminAppService;
+import com.lumanlab.parentcaringservice.admin.port.inp.web.view.res.ImpersonateUserViewRes;
+import com.lumanlab.parentcaringservice.security.UserContext;
+import com.lumanlab.parentcaringservice.user.application.service.dto.UserLoginDto;
+import com.lumanlab.parentcaringservice.user.domain.UserAgent;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/admin")
+public class AdminApi {
+
+    private final UserContext userContext;
+    private final AdminAppService adminAppService;
+
+    @PostMapping("/impersonate/{impersonateUserId}")
+    @PreAuthorize(value = "hasAnyRole('ADMIN', 'MASTER')")
+    public ImpersonateUserViewRes impersonateUser(@RequestHeader("User-Agent") UserAgent userAgent,
+                                                  @PathVariable Long impersonateUserId, HttpServletRequest request) {
+        String ip = request.getRemoteAddr();
+        Long adminUserId = userContext.getCurrentUserId().orElseThrow();
+
+        UserLoginDto dto = adminAppService.impersonateUser(adminUserId, impersonateUserId, ip, userAgent);
+
+        return new ImpersonateUserViewRes(dto);
+    }
+}
---
+++ src/test/java/com/lumanlab/parentcaringservice/admin/port/inp/web/AdminApiTest.java
@@ -0,0 +1,53 @@
+package com.lumanlab.parentcaringservice.admin.port.inp.web;
+
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.lumanlab.parentcaringservice.support.BaseApiTest;
+import com.lumanlab.parentcaringservice.support.annotation.WithMasterUser;
+import com.lumanlab.parentcaringservice.user.domain.UserAgent;
+import com.lumanlab.parentcaringservice.user.domain.UserRole;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AdminApiTest extends BaseApiTest {
+
+    @Test
+    @WithMasterUser
+    @DisplayName("사용자 임퍼소네이션")
+    void impersonate() throws Exception {
+        var targetUser = authHelper.createUser("user@example.com", "password", null, UserRole.PARENT);
+
+        mockMvc.perform(withAuth(post("/api/admin/impersonate/{userId}", targetUser.getId())
+                        .header("User-Agent", UserAgent.LUMANLAB_ADMIN))
+                )
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.accessToken").exists(),
+                        jsonPath("$.refreshToken").exists(),
+                        jsonPath("$.refreshTokenExpiredAt").exists()
+                )
+                .andDo(MockMvcRestDocumentationWrapper.document("admin-impersonation",
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("Admin")
+                                .summary("어드민 임퍼소네이션")
+                                .description("특정 유저의 인증 정보로 인증 토큰 발급")
+                                .requestHeaders(
+                                        headerWithName("User-Agent").description("사용자 에이전트 정보")
+                                )
+                                .responseFields(
+                                        fieldWithPath("accessToken").description("액세스 토큰"),
+                                        fieldWithPath("refreshToken").description("리프레시 토큰"),
+                                        fieldWithPath("refreshTokenExpiredAt").description("리프레시 토큰 만료 시간")
+                                )
+                                .build()
+                        )
+                ));
+    }
+}
